e4cfbd65e4e6ea33721245a8fea1be84daa244ea
skykid17 <geolai71@gmail.com>
2025-01-23 15:32:40 +0800

    for testing

--- logparser/NuLog/test.py
+++ logparser/NuLog/test.py
@@ -3,12 +3,11 @@
 import sys
 sys.path.append('C:/Users/georgelai/Documents/GItHub/logparser')
 
-from logparser.NuLog import LogParser
+from logparser.NuLog.NuLog_George import LogParser
 
-
-input_dir = './data/loghub_2k/HDFS/' # The input directory of log file
-output_dir = 'demo_result/'  # The output directory of parsing results
-log_file = 'HDFS_2k.log'  # The input log file name
+input_dir = './data/test_log/'
+output_dir = 'demo_result/' 
+log_file = 'nxlog.log'
 log_format = '<Date> <Time> <Pid> <Level> <Component>: <Content>'  # HDFS log format
 # Regular expression list for optional preprocessing (default: [])
 regex = [
@@ -18,7 +17,7 @@ regex = [
 ]
 filters = r"(\s+blk_)|(:)|(\s)"
 k = 15
-nr_epochs = 2 # Number of epochs to run
+nr_epochs = 1 # Number of epochs to run
 num_samples = 0
 
 parser = LogParser(log_format=log_format, indir=input_dir, outdir=output_dir, filters=filters, k=k)
